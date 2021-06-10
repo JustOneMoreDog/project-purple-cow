13a55e1c89e64e10f5d863345fe09ec577a7723e
Adam Littrell <adam.k.littrell@gmail.com>
2021-06-10 11:45:41 -0400

    an actually working poc that will tell you the url that you passed

--- ssl-check-api-endpoint/lambda_function.py
+++ ssl-check-api-endpoint/lambda_function.py
@@ -2,10 +2,21 @@ import json
 
 def lambda_handler(event, context):
     # POC function that will return the url that was passed to the api and the request itself for testing
+    # This time the POC works
+    unsupported = {
+        'statusCode': 404,
+        'body': "Unsupported"
+    }
+    # Making sure that the right api endpoint is called with the right method
+    if event["path"] == "/ssl-check" and event["httpMethod"] == "GET":
+        # Making sure that params were passed correctly
+        if "queryStringParameters" in event and "url" in event["queryStringParameters"]:
+            url = event["queryStringParameters"]["url"]
+        else:
+            return unsupported
+    else:
+        return unsupported
     return {
         'statusCode': 200,
-        'body': {
-            "your_url": event['url'],
-            "event": event    
-        },
+        'body': "URL passed was %s" % url,
     }
